7a74a6c4452149327d5ad5d79699fec15d8bf393
Antoine Poisson <Crow@localhost.localdomain>
2019-06-14 18:55:31 +0200

    [Edit] Editing Icon & Title

---
+++ src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todo</h1>
+    </header>
+  </section>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style src="./todo.css"></style>
