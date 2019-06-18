6c8a93aabca62fce703ab2b52dbe35e4d0fffe79
Antoine Poisson <Crow@localhost.localdomain>
2019-06-18 23:43:59 +0200

    [Edit] Editing Mutation function

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,64 +1,31 @@
 <template>
    <section class="todoapp">
       <header class="header">
-         <button class="adding-todo" type="box" @click.prevent="AddTodo">Ajouter</button>
-         <input type="text" class="new-todo" placeholder=" Ajouter un Todo" v-model="message" @keyup.enter="AddTodo">
+         <button class="adding-todo" type="box" @click.prevent="$store.commit('AddTodo')">Ajouter</button>
+         <input type="text" class="new-todo" placeholder=" Ajouter un Todo" v-model="$store.state.message" @keyup.enter="$store.commit('AddTodo')">
       </header>
       <ul class="todo-list" id="list">
-         <li class="todo" v-for="(list, index) in lists" v-if="list.text" :key="list.id" :class="{completed: list.done}">
+         <li class="todo" v-for="(list, index) in $store.state.lists" v-if="list.text" :key="list.id" :class="{completed: list.done}">
             <div class="view">
                <input type="checkbox" v-model="list.done" class="toggle">
             <label><input type="text" v-model="list.text"></label>
-               <button type="checkbox" class="destroy" @click.prevent="DeleteTodo(index)"></button>
+               <button type="checkbox" class="destroy" @click.prevent="$store.commit('DeleteTodo', index)"></button>
             </div>
          </li>
       </ul>
-      <footer class="footer" v-show="count">
+      <footer class="footer" v-show="$store.state.count">
          <div>
-            <span class="todo-count"> <strong>{{ count }}</strong> : Nombre de Todo</span>
-            <button class="clear-completed" type="box" @click.prevent="DeleteAllTodo">Tout Supprimer</button>
+            <span class="todo-count"> <strong>{{ $store.state.count }}</strong> : Nombre de Todo</span>
+            <button class="clear-completed" type="box" @click.prevent="$store.commit('DeleteAllTodo')">Tout Supprimer</button>
          </div>
       </footer>
    </section>
 </template>
 
 <script>
+   import store from '../store/index.js'
+
    export default {
-      data () {
-         return {
-            lists: [{
-               id : 0,
-               text: '',
-               done: false
-            }],
-            count : 0,
-            message: ''
-         }
-      },
-      methods: {
-         AddTodo: function() {
-            if (this.message == '' | this.message == ' ')
-               return {}
-            this.lists.push({
-               id : this.count,
-               text: this.message,
-               done: false
-            })
-            this.count++
-            this.message = ''
-         },
-         DeleteTodo: function(index) {
-            this.lists.splice(index, 1)
-            if (this.count > 0)
-               this.count--
-         },
-         DeleteAllTodo: function() {
-            this.lists.splice(this.lists)
-            this.count = 0
-         },
-         EditTodo: function(list) {
-            this.lists.text = list
-         }
-      }
+      store: store,
    }
 </script>
