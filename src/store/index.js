d187500413e17600f7cd2b9f2eec6e03806af53e
Antoine Poisson <Crow@localhost.localdomain>
2019-06-18 21:26:52 +0200

    [Add] Adding Store With Vuex : Functional

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import Vuex from 'vuex'
+
+export default new Vuex.Store({
+  state: {
+    lists: [{
+      id: 0,
+      text: '',
+      done: false
+    }],
+    count: 0,
+    message: 0,
+  },
+  mutations: {
+    AddTodo (state) {
+      if (state.message == '' | state.message == ' ')
+        return {}
+      state.lists.push({
+        id: state.count,
+        text: state.message,
+        done : false
+      })
+      state.count++
+      state.message = ''
+    },
+    DeleteTodo (state, index) {
+      state.lists.splice(index, 1)
+      if (state.count > 0)
+          state.count--
+    },
+    DeleteAllTodo (state) {
+      state.lists.splice(state.lists)
+      state.count = 0
+    },
+    EditTodo (state, list) {
+      state.lists.text = list
+    }
+  }
+})
